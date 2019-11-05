683c3a44da6ac05fbdec3286805677ffd57b5351
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-11-05 21:20:41 +0200

    adding a web server to server the static built files

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const app = express();
+const publicPath = path.join(__dirname, '..', 'build');
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'));
+ });
+ const PORT = process.env.PORT || 3001
+
+ app.listen(PORT, () => {
+     console.log(`listening on port ${PORT} ...`)
+ })
