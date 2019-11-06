498161b1e3611e88b0574bdcbdb7e54aef2b9871
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-11-06 09:33:47 +0200

    removing the build folder from .gitignore

--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ dotenv.config();
 const app = express();
 const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
+const index = path.join(publicPath, 'index.html')
 app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-    console.log(path.join('>>>>', publicPath, 'index.html'))
+    res.sendFile(path.join(index));
+    console.log(path.join('>>>>', index))
  });
  const PORT = process.env.PORT || 3001
 
