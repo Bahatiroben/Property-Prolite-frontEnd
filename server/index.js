dac94e71a88143d5837460a82328f2658dc8ba7f
BAHATI Robben <lux@BAHATIs-MacBook-Pro.local>
2019-11-06 09:17:31 +0200

    adding a debugging console

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const publicPath = path.join(__dirname, '..', 'build');
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
+    console.log(path.join('>>>>', publicPath, 'index.html'))
  });
  const PORT = process.env.PORT || 3001
 
